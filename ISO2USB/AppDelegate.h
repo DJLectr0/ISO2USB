347e671006b9b5c183c28ba1fddd257aeef9619d
Leonardo Galli <leonardogalli@Leonardos-MacBook-Pro.local>
2014-03-01 17:15:50 +0100

    Initial Commit

---
+++ ISO2USB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ISO2USB
+//
+//  Created by Leonardo Galli on 01.03.14.
+//  Copyright (c) 2014 Leonardo Galli. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
